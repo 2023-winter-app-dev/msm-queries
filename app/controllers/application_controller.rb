028b1d3c44d516a17d5509f243343a727dd5bd60
cwen-booth <cwen@chicagobooth.edu>
2023-02-06 07:41:17 +0000

    Done everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def characters
+    render({:template => "misc_templates/characters"})
+  end
+
+  def movies
+    render({:template => "misc_templates/movies"})
+  end
+
+  def directors
+    render({:template => "misc_templates/directors"})
+  end
+
+  def actors
+    render({:template => "misc_templates/actors"})
+  end
+
+  def movies_detail
+    render({:template => "misc_templates/movies_detail"})
+  end
+
+  def directors_detail
+    render({:template => "misc_templates/directors_detail"})
+  end
+
+  def actors_detail
+    render({:template => "misc_templates/actors_detail"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  get("/actors", { :controller => "application", :action => "actors" })
+  get("/characters", { :controller => "application", :action => "characters" })
+  get("/directors", { :controller => "application", :action => "directors" })
+  get("/movies", { :controller => "application", :action => "movies" })
+  
+  get("/actors/:actor_id", { :controller => "application", :action => "actors_detail" })
+  get("/characters/:character_id", { :controller => "application", :action => "characters_detail" })
+  get("/directors/:director_id", { :controller => "application", :action => "directors_detail" })
+  get("/movies/:movie_id", { :controller => "application", :action => "movies_detail" })
 end
